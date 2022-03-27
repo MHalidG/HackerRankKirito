5f1fbcd5ec04822fcc63ba56ca4efd3655201c3e
Kirito <halid.gundz@gmail.com>
2022-03-27 13:22:18 +0200

    v2

---
+++ src/Rank1/Rank5.java
@@ -0,0 +1,34 @@
+package Rank1;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Rank5 {
+    public static void main(String[] args) {
+        Scanner  sc=new Scanner(System.in);
+        int x=sc.nextInt();
+
+
+if (x>=2&&x<=20){
+    for (int i = 1; i <11 ; i++) {
+        System.out.println(x+" x "+i +" = " +(2*i));
+
+    }
+
+
+}
+else{
+    System.out.println("nayn da goood");
+}
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Rank1/Rank6.java
@@ -0,0 +1,7 @@
+package Rank1;
+
+public class Rank6 {
+    public static void main(String[] args) {
+
+    }
+}
